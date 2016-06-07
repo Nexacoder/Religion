274ed959647113fbe1200631b86f9206b2c0e770
Carson Osborn <caosborn204@gmail.com>
2016-06-06 21:27:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-$(document).ready(function() {
-var stickyNavTop = $('.nav').offset().top;
- 
-var stickyNav = function(){
-var scrollTop = $(window).scrollTop();
-      
-if (scrollTop > stickyNavTop) { 
-    $('.nav').addClass('sticky');
-} else {
-    $('.nav').removeClass('sticky'); 
-}
-};
- 
-stickyNav();
- 
-$(window).scroll(function() {
-    stickyNav();
-});
-});
-
 (function() {
      'use strict';
     // Feature Test
